6523028356157c27a0aa4fd767ec5155683ae4c3
Evandro <vando86206@gmail.com>
2025-10-03 17:33:26 -0300

    alter def identidadeIgualdade

--- fundamentos/exemplos/02.3-operadores_de_identidade.py
+++ fundamentos/exemplos/02.3-operadores_de_identidade.py
@@ -1,46 +1,44 @@
 linguagens = ["python", "java", "javascript"]
 linguagens2 = ["python", "java", "javascript"]
 linguagens3 = linguagens
+linguagensDiff = ["python 3"]
 
 
 def identidadeIS(list1, list2):
-   if list1 is list2:
-      print("\nOs ojetos são o mesmo objeto na memória!")
-   else: 
-      print("\nOs ojetos não são o mesmo objeto, apenas têm valores iguais!")
+    if list1 is list2:
+        print("\nSão o mesmo objeto na memória!")
+    else:
+        print("\nNão são o mesmo objeto, apenas têm valores iguais!")
 
 
 def identidadeIS_not(list1, list2):
     if list1 is not list2:
-        print(f"\nOs ojetos não são o mesmo objeto na memória!")
-    else: 
-        print("\nOs ojetos são o mesmo objeto na memória!")
+        print(f"\nNão são o mesmo objeto na memória!")
+    else:
+        print("\nSão o mesmo objeto na memória!")
 
 
 def identidadeIgualdade(list1, list2):
     if list1 == list2:
         print("\nOs ojetos têm valores iguais!")
-    else: 
+    else:
         print("\nOs ojetos têm valores diferentes!")
 
 
-print(45*"_")
-print("Operador de identidade 'is'")
-identidadeIS(linguagens, linguagens2)
-identidadeIS(linguagens, linguagens3)
-print(45*"_")
+# print(45 * "_")
+# print("Operador de identidade 'is'")
+# identidadeIS(linguagens, linguagens2)
+# identidadeIS(linguagens, linguagens3)
+# print(45 * "_")
 
-print(45*"*")
-print("Operador de identidade 'is not'")
-identidadeIS_not(linguagens, linguagens2)
-identidadeIS_not(linguagens, linguagens3)
-print(45*"_")
+# print(45*"*")
+# print("Operador de identidade 'is not'")
+# identidadeIS_not(linguagens, linguagens2)
+# identidadeIS_not(linguagens, linguagens3)
+# print(45*"_")
 
-print(45*"_")
+print(45 * "_")
 print("Operador de igualdade '=='")
 identidadeIgualdade(linguagens, linguagens2)
-identidadeIgualdade(linguagens, linguagens3)
-print(45*"_")
-
-
-
+identidadeIgualdade(linguagens, linguagensDiff)
+print(45 * "_")
