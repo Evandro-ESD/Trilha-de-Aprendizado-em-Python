64cf5442ad5ee9a492c3464550310bebfeb345cb
Evandro <vando86206@gmail.com>
2025-10-03 10:41:44 -0300

    Exercício calculadora

---
+++ criar_venv.md
@@ -0,0 +1,23 @@
+## Na pasta do projeto
+
+# crie um ambiente virtual chamado .venv com o comando
+
+python -m venv .venv
+
+# ativar ambiente virtual
+
+.venv\Scripts\Activate
+
+# ativar ambiente virtual no linux/Mac
+
+source .venv/bin/activate
+
+## Criar arquivo requirements.txt para guardar as bibliotecas que seu projeto usa. Crie ele na raiz do projeto com, por exemplo:
+
+numpy
+pandas
+matplotlib
+
+## Para instalar tudo:
+
+pip install -r requirements.txt
--- fundamentos/exercicios/01-calculadora.py
+++ fundamentos/exercicios/01-calculadora.py
@@ -1,3 +1,29 @@
 # Exercício: Calculadora
 
-# ...implemente uma calculadora simples aqui...
+
+class Calculadora:
+    def __init__(self):
+        self.num1 = int(input("Insira o 1º número: "))
+        self.operacao = input('Insira a operação ( "+" | "-" | "*" | "/" ): ')
+        self.num2 = int(input("Insira o 1º número: "))
+
+    def calculo(self):
+        match self.operacao:
+            case "+":
+                return self.num1 + self.num2
+            case "-":
+                return self.num1 - self.num2
+            case "*":
+                return self.num1 * self.num2
+            case "/":
+                return (
+                    "Erro: divisão por zero!"
+                    if self.num2 == 0
+                    else self.num1 / self.num2
+                )
+            case _:
+                return "Operação invalida!"
+
+
+calc = Calculadora()
+print("Resulatdo: ", calc.calculo())
