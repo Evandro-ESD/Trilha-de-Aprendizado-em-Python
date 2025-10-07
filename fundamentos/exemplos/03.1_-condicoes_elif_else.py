7da35497c55b3454aa2ca95e8f4e451a7b76a761
Evandro <vando86206@gmail.com>
2025-10-07 13:26:00 -0300

    exemplos básicos de for e while

--- fundamentos/exemplos/03.1_-condicoes_elif_else.py
+++ fundamentos/exemplos/03.1_-condicoes_elif_else.py
@@ -1,41 +1,41 @@
-# Referencia: https://www.w3schools.com/python/python_conditions.asp
+# # Referencia: https://www.w3schools.com/python/python_conditions.asp
 
 
-##################################################
-##################################################
-#######                                    #######
-####### DESCOMENTAR CADA BLOCO PARA TESTAR #######
-#######                                    #######
-##################################################
-##################################################
+# ##################################################
+# ##################################################
+# #######                                    #######
+# ####### DESCOMENTAR CADA BLOCO PARA TESTAR #######
+# #######                                    #######
+# ##################################################
+# ##################################################
 
-##################################################
-###################### elif ######################
-##################################################
+# ##################################################
+# ###################### elif ######################
+# ##################################################
 
-# Se a condição if não for verdadeira o python vsi tentar elif (se não se)
+# # Se a condição if não for verdadeira o python vsi tentar elif (se não se)
 
-a = 4
-b = 4
-if b > a:
-    print("b é maior que a")
-elif a < b:
-    print("b é menor que a")
-elif a == b:
-    print("a e b são iguais")
+# a = 4
+# b = 4
+# if b > a:
+#     print("b é maior que a")
+# elif a < b:
+#     print("b é menor que a")
+# elif a == b:
+#     print("a e b são iguais")
 
 
-##################################################
-###################### else ######################
-##################################################
+# ##################################################
+# ###################### else ######################
+# ##################################################
 
-# Captura qualquer coisa que não seja capturada anteriormente
+# # Captura qualquer coisa que não seja capturada anteriormente
 
-a = 5
-b = 4
-if b > a:
-    print("b é maior que a")
-elif a == b:
-    print("a e b são iguais")
-else:
-    print("a e b são diferentes")
+# a = 5
+# b = 4
+# if b > a:
+#     print("b é maior que a")
+# elif a == b:
+#     print("a e b são iguais")
+# else:
+#     print("a e b são diferentes")
--- fundamentos/exemplos/08-list_comprehensions.py
+++ fundamentos/exemplos/08-list_comprehensions.py
@@ -1,3 +1,21 @@
 # Exemplo: List Comprehensions
+# Referência: leandrohirt.oficial instagram
 
-# ...adicione exemplos de list comprehensions aqui...
+produtos = [
+    {"nome": "celular", "vendas": 230},
+    {"nome": "Notebook", "vendas": 188},
+    {"nome": "Tablet", "vendas": 75},
+    {"nome": "Smartwath", "vendas": 95},
+    {"nome": "Fone de ouvido", "vendas": 310},
+]
+
+mais_vendidos = [produto["nome"] for produto in produtos if produto["vendas"] > 300]
+
+print(mais_vendidos)
+
+# ================================================
+mais_vendidos_so_o_nome = next(
+    (produto["nome"] for produto in produtos if produto["vendas"] > 300), None
+)
+
+print(mais_vendidos_so_o_nome)
