aa65f81c01a3907def1b95dde56bc7e0e386f4d5
Evandro <vando86206@gmail.com>
2025-10-18 22:57:44 -0300

    exemplo simples de herança

--- 2-orientacao-objetos/exemplos/04-poo_heranca.py
+++ 2-orientacao-objetos/exemplos/04-poo_heranca.py
@@ -12,3 +12,27 @@ class veiculo:
 print(50 * "-")
 c = veiculo("BYD", "SONG PRO")
 c.showInfos()
+
+# ___________________________________
+print(50 * "-")
+
+
+class Carro(veiculo):
+    def __init__(self, marca, modelo):
+        super().__init__(marca, modelo)
+
+    def showInfos(self):
+        msg = "Infomações da classe Carro - Usando Herança da classe Veículo"
+        msg2 = "\n" + self.marca + " / " + self.modelo
+        print(msg, msg2)
+
+        # # Exemplo sem reescrever o método:
+        # # descomentar a próxima linha para usar e comenatar as linhs 25, 26 e 27
+        # return super().showInfos()
+
+
+c1 = Carro("Nissan", "Tiida")
+c1.showInfos()
+
+# ___________________________________
+print(50 * "-")
